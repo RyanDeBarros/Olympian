961f837def72e7d59eb3f11b921803f49dd4255d
RyanDeBarros <ryan2004db@gmail.com>
2025-03-12 16:55:39 -0400

    Project setup.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Olympian
-OpenGL engine/framework to make future graphics projects easier, faster, and safer.
+Olympian Engine is an GLFW/OpenGL framework to make future graphics projects easier, faster, and safer. For this project, I'm also writing a [design manual](https://docs.google.com/document/d/186rgx-V7eJ7qQJLzzKFEDsNHN2lNX5ptwvy6lPVgLIU/edit?usp=sharing), at least for the planning phase, so that it can be better designed.
+
+## Licensing
+This project uses the following libraries:
+
+- **nigels-com/glew**
+- **GLFW**
+- **g-truc/glm**
+- **nothings/stb**
+
+Additionally, extra assets such as fonts or images may be used.
+For the actual licensing information, refer to the licenses folder.
---
+++ src/Olympian.cpp
@@ -0,0 +1,34 @@
+﻿#include "Olympian.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	if (glfwInit() != GLFW_TRUE)
+		return 1;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	GLFWwindow* window = glfwCreateWindow(1440, 1080, "Olympian Engine", nullptr, nullptr);
+	if (!window)
+		return 2;
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+		return 3;
+	glClearColor(0.2f, 0.5f, 0.8f, 1.0f);
+	glfwSwapInterval(1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	glfwTerminate();
+	return 0;
+}
