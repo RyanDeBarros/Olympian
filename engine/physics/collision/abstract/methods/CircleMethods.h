789d6f66407246b458bd495475fef244c07343a2
RyanDeBarros <ryan2004db@gmail.com>
2025-06-01 15:42:02 -0400

    Made circle methods independent of polygon winding order.

--- engine/physics/collision/abstract/methods/CircleMethods.h
+++ engine/physics/collision/abstract/methods/CircleMethods.h
@@ -10,33 +10,56 @@ namespace oly::acm2d::internal
 	template<typename Polygon>
 	inline OverlapResult circle_overlaps_polygon(const Circle& c, const Polygon& polygon)
 	{
+		std::optional<bool> ccw;
+		glm::vec2 prev = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), polygon[polygon.size() - 1]);
 		for (size_t i = 0; i < polygon.size(); ++i)
 		{
-			glm::vec2 a = polygon[i];
-			a = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), a);
-			glm::vec2 b = polygon[(i + 1) % polygon.size()];
-			b = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), b);
+			glm::vec2 a = prev;
+			glm::vec2 b = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), polygon[i]);
+			prev = b;
 
 			UnitVector2D edge = b - a;
 			UnitVector2D axis = edge.get_quarter_turn();
 			float center_proj = axis.dot(c.center);
 			float segment_proj = axis.dot(a);
-			if (segment_proj < center_proj - c.radius) // circle is left of edge
-				return false;
-			else if (segment_proj <= center_proj + c.radius) // circle may intersect edge
-			{
-				float cross = math::cross(edge, c.center - a);
-				float discriminant = c.radius * c.radius - cross * cross;
-				if (discriminant >= 0.0f)
+			static const auto circle_intersects_edge = [](const Circle& c, const UnitVector2D& edge, glm::vec2 a, glm::vec2 b)
 				{
-					float offset = edge.dot(c.center - a);
-					discriminant = glm::sqrt(discriminant);
-					float t1 = offset - discriminant;
-					float t2 = offset + discriminant;
+					float cross = math::cross(edge, c.center - a);
+					float discriminant = c.radius * c.radius - cross * cross;
+					if (discriminant >= 0.0f)
+					{
+						float offset = edge.dot(c.center - a);
+						discriminant = glm::sqrt(discriminant);
+						float t1 = offset - discriminant;
+						float t2 = offset + discriminant;
 
-					if (t2 >= 0.0f && t1 <= glm::length(b - a)) // circle intersects edge
-						return true;
-				}
+						if (t2 >= 0.0f && t1 <= glm::length(b - a))
+							return true;
+					}
+					return false;
+				};
+			if (!ccw.has_value())
+			{
+				if (segment_proj < center_proj - c.radius)
+					ccw = true;
+				else if (segment_proj > center_proj + c.radius)
+					ccw = false;
+				else if (circle_intersects_edge(c, edge, a, b))
+					return true;
+			}
+			else if (ccw.value())
+			{
+				if (segment_proj < center_proj - c.radius) // circle is left of edge
+					return false;
+				else if (circle_intersects_edge(c, edge, a, b))
+					return true;
+			}
+			else
+			{
+				if (segment_proj > center_proj + c.radius) // circle is right of edge
+					return false;
+				else if (circle_intersects_edge(c, edge, a, b))
+					return true;
 			}
 		}
 		return true;
@@ -46,27 +69,90 @@ namespace oly::acm2d::internal
 	inline CollisionResult circle_collides_polygon(const Circle& c, const Polygon& polygon)
 	{
 		CollisionResult info{ .overlap = true, .penetration_depth = std::numeric_limits<float>::max() };
+		std::optional<bool> ccw;
+		glm::vec2 prev = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), polygon[polygon.size() - 1]);
+		float ccw_depth = std::numeric_limits<float>::max();
+		UnitVector2D ccw_unit_impulse;
+		float cw_depth = std::numeric_limits<float>::max();
+		UnitVector2D cw_unit_impulse;
 		for (size_t i = 0; i < polygon.size(); ++i)
 		{
-			glm::vec2 a = polygon[i];
-			a = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), a);
-			glm::vec2 b = polygon[(i + 1) % polygon.size()];
-			b = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), b);
+			glm::vec2 a = prev;
+			glm::vec2 b = transform_point(acm2d::internal::CircleGlobalAccess::get_ginv(c), polygon[i]);
+			prev = b;
 
 			UnitVector2D edge = b - a;
 			UnitVector2D axis = edge.get_quarter_turn();
 			float center_proj = axis.dot(c.center);
 			float segment_proj = axis.dot(a);
-			if (segment_proj < center_proj - c.radius) // circle is left of edge
-				return { .overlap = false };
+			if (!ccw.has_value())
+			{
+				float _ccw_depth = center_proj + c.radius - segment_proj;
+				if (_ccw_depth < ccw_depth)
+				{
+					ccw_depth = _ccw_depth;
+					ccw_unit_impulse = -axis;
+				}
+				float _cw_depth = segment_proj - center_proj + c.radius;
+				if (_cw_depth < cw_depth)
+				{
+					cw_depth = _cw_depth;
+					cw_unit_impulse = axis;
+				}
+				// set winding order
+				if (segment_proj < center_proj - c.radius)
+				{
+					ccw = true;
+					info.penetration_depth = ccw_depth;
+					info.unit_impulse = ccw_unit_impulse;
+				}
+				else if (segment_proj > center_proj + c.radius)
+				{
+					ccw = false;
+					info.penetration_depth = cw_depth;
+					info.unit_impulse = cw_unit_impulse;
+				}
+			}
+			else if (ccw.value())
+			{
+				if (segment_proj < center_proj - c.radius) // circle is left of edge
+					return { .overlap = false };
+				else
+				{
+					float inward_depth = center_proj + c.radius - segment_proj;
+					if (inward_depth < info.penetration_depth)
+					{
+						info.penetration_depth = inward_depth;
+						info.unit_impulse = -axis;
+					}
+				}
+			}
 			else
 			{
-				float inward_depth = center_proj + c.radius - segment_proj;
-				if (inward_depth < info.penetration_depth)
+				if (segment_proj > center_proj + c.radius) // circle is right of edge
+					return { .overlap = false };
+				else
 				{
-					info.penetration_depth = inward_depth;
-					info.unit_impulse = -axis;
+					float inward_depth = segment_proj - center_proj + c.radius;
+					if (inward_depth < info.penetration_depth)
+					{
+						info.penetration_depth = inward_depth;
+						info.unit_impulse = axis;
+					}
 				}
+			}
+		}
+		if (!ccw.has_value())
+		{
+			if (ccw_depth < cw_depth)
+			{
+				info.penetration_depth = ccw_depth;
+				info.unit_impulse = ccw_unit_impulse;
+			}
+			else
+			{
+				info.penetration_depth = cw_depth;
+				info.unit_impulse = cw_unit_impulse;
 			}
 		}
 		if (!internal::CircleGlobalAccess::has_no_global(c))
