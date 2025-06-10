be529a42d6e04c6096157f738c8df47e613bacab
RyanDeBarros <ryan2004db@gmail.com>
2025-06-09 23:26:01 -0400

    RAII for framebuffers.

---
+++ engine/graphics/backend/basic/Framebuffers.cpp
@@ -0,0 +1,117 @@
+#include "Framebuffers.h"
+
+namespace oly::graphics
+{
+	Framebuffer::Framebuffer()
+	{
+		glGenFramebuffers(1, &id);
+	}
+
+	Framebuffer::Framebuffer(Framebuffer&& other) noexcept
+		: id(other.id)
+	{
+		other.id = 0;
+	}
+
+	Framebuffer::~Framebuffer()
+	{
+		glDeleteFramebuffers(1, &id);
+	}
+
+	Framebuffer& Framebuffer::operator=(Framebuffer&& other) noexcept
+	{
+		if (this != &other)
+		{
+			glDeleteFramebuffers(1, &id);
+			id = other.id;
+			other.id = 0;
+		}
+		return *this;
+	}
+
+	void Framebuffer::attach_2d_texture(ColorAttachment a, GLuint texture, GLint level)
+	{
+		glNamedFramebufferTexture(id, a, texture, level);
+		if (std::find(color_attachments.begin(), color_attachments.end(), a) == color_attachments.end())
+			color_attachments.push_back(a);
+	}
+
+	void Framebuffer::attach_2d_texture(NonColorAttachment a, GLuint texture, GLint level)
+	{
+		glNamedFramebufferTexture(id, a, texture, level);
+		if (a == NonColorAttachment::depth())
+			depth_attached = true;
+		else if (a == NonColorAttachment::stencil())
+			stencil_attached = true;
+		else if (a == NonColorAttachment::depth_stencil())
+			depth_stencil_attached = true;
+	}
+
+	void Framebuffer::attach_3d_texture(ColorAttachment a, GLuint texture, GLint layer, GLint level)
+	{
+		glNamedFramebufferTextureLayer(id, a, texture, level, layer);
+		if (std::find(color_attachments.begin(), color_attachments.end(), a) == color_attachments.end())
+			color_attachments.push_back(a);
+	}
+
+	void Framebuffer::attach_3d_texture(NonColorAttachment a, GLuint texture, GLint layer, GLint level)
+	{
+		glNamedFramebufferTextureLayer(id, a, texture, level, layer);
+		if (a == NonColorAttachment::depth())
+			depth_attached = true;
+		else if (a == NonColorAttachment::stencil())
+			stencil_attached = true;
+		else if (a == NonColorAttachment::depth_stencil())
+			depth_stencil_attached = true;
+	}
+
+	void Framebuffer::detach_texture(ColorAttachment a)
+	{
+		glNamedFramebufferTexture(id, a, 0, 0);
+		auto it = std::find(color_attachments.begin(), color_attachments.end(), a);
+		if (it != color_attachments.end())
+			color_attachments.erase(it);
+	}
+
+	void Framebuffer::detach_texture(NonColorAttachment a)
+	{
+		glNamedFramebufferTexture(id, a, 0, 0);
+		if (a == NonColorAttachment::depth())
+			depth_attached = false;
+		else if (a == NonColorAttachment::stencil())
+			stencil_attached = false;
+		else if (a == NonColorAttachment::depth_stencil())
+			depth_stencil_attached = false;
+	}
+
+	Framebuffer::Status Framebuffer::status(Target target) const
+	{
+		return (Status)glCheckNamedFramebufferStatus(id, (GLenum)target);
+	}
+
+	void Framebuffer::draw_buffer(ColorAttachment color) const
+	{
+		glNamedFramebufferDrawBuffer(id, color);
+	}
+
+	void Framebuffer::draw_buffers(const ColorAttachment* colors, size_t count) const
+	{
+		glNamedFramebufferDrawBuffers(id, count, reinterpret_cast<const GLenum*>(colors));
+	}
+	
+	void Framebuffer::draw_buffers() const
+	{
+		if (!color_attachments.empty())
+			glNamedFramebufferDrawBuffers(id, color_attachments.size(), reinterpret_cast<const GLenum*>(color_attachments.data()));
+	}
+
+	void Framebuffer::read_buffer(ColorAttachment color) const
+	{
+		glNamedFramebufferReadBuffer(id, color);
+	}
+
+	void Framebuffer::blit(const Framebuffer& read, const Framebuffer& draw, math::IRect2D src, math::IRect2D dst, BlitMask mask, BlitFilter filter)
+	{
+		glBlitNamedFramebuffer(read, draw, src.x1, src.y1, src.x2, src.y2, dst.x1, dst.y1, dst.x2, dst.y2, (GLenum)mask, (GLenum)filter);
+	}
+}
---
+++ engine/graphics/backend/basic/Framebuffers.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "graphics/backend/basic/Textures.h"
+#include "core/math/Shapes.h"
+
+namespace oly::graphics
+{
+	class Framebuffer
+	{
+		GLuint id = 0;
+
+	public:
+		Framebuffer();
+		Framebuffer(const Framebuffer&) = delete;
+		Framebuffer(Framebuffer&&) noexcept;
+		~Framebuffer();
+		Framebuffer& operator=(Framebuffer&&) noexcept;
+
+		operator GLuint () const { return id; }
+
+		enum class Target
+		{
+			REG  = GL_FRAMEBUFFER,
+			DRAW = GL_DRAW_FRAMEBUFFER,
+			READ = GL_READ_FRAMEBUFFER
+		};
+		void bind(Target target = Framebuffer::Target::REG) const { glBindFramebuffer((GLenum)target, id); }
+
+		class NonColorAttachment
+		{
+			GLenum a;
+
+			constexpr explicit NonColorAttachment(GLenum a) : a(a) {}
+
+		public:
+			static constexpr NonColorAttachment depth() { return NonColorAttachment(GL_DEPTH_ATTACHMENT); }
+			static constexpr NonColorAttachment stencil() { return NonColorAttachment(GL_STENCIL_ATTACHMENT); }
+			static constexpr NonColorAttachment depth_stencil() { return NonColorAttachment(GL_DEPTH_STENCIL_ATTACHMENT); }
+
+			constexpr operator GLenum () const { return a; }
+		};
+
+		class ColorAttachment
+		{
+			GLenum a;
+
+			constexpr explicit ColorAttachment(GLenum a) : a(a) {}
+
+		public:
+			static constexpr ColorAttachment color(GLuint i) { return ColorAttachment(GL_COLOR_ATTACHMENT0 + i); }
+			static GLuint max_color_attachments() { GLint v; glGetIntegerv(GL_MAX_COLOR_ATTACHMENTS, &v); return v; }
+
+			constexpr operator GLenum () const { return a; }
+		};
+
+		void attach_2d_texture(ColorAttachment a, GLuint texture, GLint level = 0);
+		void attach_2d_texture(NonColorAttachment a, GLuint texture, GLint level = 0);
+		void attach_3d_texture(ColorAttachment a, GLuint texture, GLint layer, GLint level = 0);
+		void attach_3d_texture(NonColorAttachment a, GLuint texture, GLint layer, GLint level = 0);
+		void detach_texture(ColorAttachment a);
+		void detach_texture(NonColorAttachment a);
+
+		enum class Status
+		{
+			COMPLETE = GL_FRAMEBUFFER_COMPLETE,
+			UNDEFINED = GL_FRAMEBUFFER_UNDEFINED,
+			INCOMPLETE_ATTACHMENT = GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT,
+			INCOMPLETE_MISSING_ATTACHMENT = GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT,
+			INCOMPLETE_DRAW_BUFFER = GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER,
+			INCOMPLETE_READ_BUFFER = GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER,
+			UNSUPPORTED = GL_FRAMEBUFFER_UNSUPPORTED,
+			INCOMPLETE_MULTISAMPLE = GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE,
+			INCOMPLETE_LAYER_TARGETS = GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS
+		};
+		Status status(Target target = Target::REG) const;
+
+		void draw_buffer(ColorAttachment a) const;
+		void draw_buffers(const ColorAttachment* as, size_t count) const;
+		void draw_buffers() const;
+
+		void read_buffer(ColorAttachment color) const;
+
+		enum class BlitMask
+		{
+			COLOR = GL_COLOR_BUFFER_BIT,
+			DEPTH = GL_DEPTH_BUFFER_BIT,
+			STENCIL = GL_STENCIL_BUFFER_BIT
+		};
+		enum class BlitFilter
+		{
+			NEAREST = GL_NEAREST,
+			LINEAR = GL_LINEAR
+		};
+		static void blit(const Framebuffer& read, const Framebuffer& draw, math::IRect2D src, math::IRect2D dst, BlitMask mask, BlitFilter filter);
+
+	private:
+		std::vector<ColorAttachment> color_attachments;
+		bool depth_attached = false, stencil_attached = false, depth_stencil_attached = false;
+	};
+
+	inline void unbind_framebuffer(Framebuffer::Target target = Framebuffer::Target::REG) { glBindFramebuffer((GLenum)target, 0); }
+
+	// TODO FramebufferBlock
+	// TODO Renderbuffer
+}
