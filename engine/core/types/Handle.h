c0decc20154986a55b106cb03a587ad49ad58ff1
RyanDeBarros <ryan2004db@gmail.com>
2025-07-17 23:09:11 -0400

    Further optimized HandlePool.

--- engine/core/types/Handle.h
+++ engine/core/types/Handle.h
@@ -13,13 +13,14 @@ namespace oly
 	namespace internal
 	{
 		// LATER multi-threading and thead safety
+		// TODO replace many uses of shared_ptr with Handle system.
 		template<typename Object>
 		class HandlePool
 		{
 			std::vector<Object> objects;
 			std::stack<size_t> unoccupied;
 			std::unordered_set<size_t> marked_for_deletion;
-			std::vector<std::unordered_set<Handle<Object>*>> references;
+			std::vector<Handle<Object>*> reference_heads;
 
 			HandlePool() = default;
 			HandlePool(const HandlePool<Object>&) = delete;
@@ -43,7 +44,7 @@ namespace oly
 				if (unoccupied.empty())
 				{
 					objects.push_back(obj);
-					references.push_back({});
+					reference_heads.push_back(nullptr);
 					return objects.size() - 1;
 				}
 				else
@@ -70,26 +71,9 @@ namespace oly
 				return marked_for_deletion.count(idx);
 			}
 			
-			void increment_references(size_t idx, Handle<Object>* handle)
-			{
-				references[idx].insert(handle);
-			}
-			
-			void decrement_references(size_t idx, Handle<Object>* handle)
-			{
-				references[idx].erase(handle);
-				if (references[idx].empty())
-				{
-					marked_for_deletion.erase(idx);
-					unoccupied.push(idx);
-				}
-			}
-			
-			void swap_references(size_t idx, Handle<Object>* existing, Handle<Object>* with)
-			{
-				if (references[idx].erase(existing))
-					references[idx].insert(with);
-			}
+			void increment_references(size_t idx, Handle<Object>* handle);
+			void decrement_references(size_t idx, Handle<Object>* handle);
+			void swap_references(size_t idx, Handle<Object>* existing, Handle<Object>* with);
 		};
 	}
 
@@ -99,8 +83,9 @@ namespace oly
 	private:
 		friend class internal::HandlePool<Object>;
 
-		bool valid = false;
 		size_t pool_idx = size_t(-1);
+		Handle<Object>* prev_adj_reference = nullptr;
+		Handle<Object>* next_adj_reference = nullptr;
 
 	public:
 		Handle(bool init = false)
@@ -110,25 +95,24 @@ namespace oly
 		}
 
 		Handle(const Handle<Object>& other)
-			: valid(other.valid), pool_idx(other.pool_idx)
+			: pool_idx(other.pool_idx)
 		{
 			pool().increment_references(pool_idx, this);
 		}
 
 		Handle(Handle<Object>&& other) noexcept
-			: valid(other.valid), pool_idx(other.pool_idx)
+			: pool_idx(other.pool_idx)
 		{
 			pool().swap_references(pool_idx, &other, this);
-			other.valid = false;
 		}
 		
 		Handle<Object>& operator=(const Handle<Object>& other)
 		{
 			if (this != &other)
 			{
-				if (valid)
+				if (valid())
 				{
-					if (other.valid)
+					if (other.valid())
 					{
 						if (pool_idx != other.pool_idx)
 						{
@@ -138,18 +122,14 @@ namespace oly
 						}
 					}
 					else
-					{
 						pool().decrement_references(pool_idx, this);
-						valid = false;
-					}
 				}
 				else
 				{
-					if (other.valid)
+					if (other.valid())
 					{
 						pool_idx = other.pool_idx;
 						pool().increment_references(pool_idx, this);
-						valid = true;
 					}
 				}
 			}
@@ -160,37 +140,28 @@ namespace oly
 		{
 			if (this != &other)
 			{
-				if (valid)
+				if (valid())
 				{
-					if (other.valid)
+					if (other.valid())
 					{
 						if (pool_idx != other.pool_idx)
 						{
 							pool().decrement_references(pool_idx, this);
 							pool_idx = other.pool_idx;
 							pool().swap_references(pool_idx, &other, this);
-							other.valid = false;
 						}
 						else
-						{
 							pool().decrement_references(pool_idx, &other);
-							other.valid = false;
-						}
 					}
 					else
-					{
 						pool().decrement_references(pool_idx, this);
-						valid = false;
-					}
 				}
 				else
 				{
-					if (other.valid)
+					if (other.valid())
 					{
 						pool_idx = other.pool_idx;
 						pool().swap_references(pool_idx, &other, this);
-						valid = true;
-						other.valid = false;
 					}
 				}
 			}
@@ -204,7 +175,7 @@ namespace oly
 
 		const Object& operator*() const
 		{
-			if (valid)
+			if (valid())
 				return pool().objects[pool_idx];
 			else
 				throw Error(ErrorCode::NULL_POINTER);
@@ -212,7 +183,7 @@ namespace oly
 		
 		Object& operator*()
 		{
-			if (valid)
+			if (valid())
 				return pool().objects[pool_idx];
 			else
 				throw Error(ErrorCode::NULL_POINTER);
@@ -220,7 +191,7 @@ namespace oly
 		
 		const Object* operator->() const
 		{
-			if (valid)
+			if (valid())
 				return &pool().objects[pool_idx];
 			else
 				return nullptr;
@@ -228,7 +199,7 @@ namespace oly
 		
 		Object* operator->()
 		{
-			if (valid)
+			if (valid())
 				return &pool().objects[pool_idx];
 			else
 				return nullptr;
@@ -236,27 +207,31 @@ namespace oly
 
 		operator bool() const
 		{
-			return valid;
+			return valid();
 		}
 		
-		bool is_valid() const
+		bool valid() const
 		{
-			return valid;
+			if (next_adj_reference)
+				return true;
+			if (pool_idx < pool().reference_heads.size())
+				return pool().reference_heads[pool_idx] == this;
+			else
+				return false;
 		}
 		
 		void init()
 		{
-			if (valid)
+			if (valid())
 				pool().decrement_references(pool_idx, this);
 
 			pool_idx = pool().init_slot();
 			pool().increment_references(pool_idx, this);
-			valid = true;
 		}
 		
 		void clone()
 		{
-			if (valid)
+			if (valid())
 			{
 				Object copy = pool().objects[pool_idx];
 				pool().decrement_references(pool_idx, this);
@@ -268,9 +243,8 @@ namespace oly
 		Handle<Object> get_clone() const
 		{
 			Handle<Object> cloned;
-			if (valid)
+			if (valid())
 			{
-				cloned.valid = true;
 				cloned.pool_idx = pool().init_slot(pool().objects[pool_idx]);
 				pool().increment_references(cloned.pool_idx, &cloned);
 			}
@@ -279,7 +253,7 @@ namespace oly
 		
 		void mark_for_deletion() const
 		{
-			if (valid)
+			if (valid())
 				pool().mark_for_deletion(pool_idx);
 			else
 				throw Error(ErrorCode::NULL_POINTER);
@@ -287,7 +261,7 @@ namespace oly
 		
 		bool is_marked_for_deletion() const
 		{
-			if (valid)
+			if (valid())
 				return pool().is_marked_for_deletion(pool_idx);
 			else
 				throw Error(ErrorCode::NULL_POINTER);
@@ -295,7 +269,7 @@ namespace oly
 		
 		void unmark_for_deletion() const
 		{
-			if (valid)
+			if (valid())
 				pool().unmark_for_deletion(pool_idx);
 			else
 				throw Error(ErrorCode::NULL_POINTER);
@@ -303,9 +277,8 @@ namespace oly
 		
 		void invalidate()
 		{
-			if (valid)
+			if (valid())
 				pool().decrement_references(pool_idx, this);
-			valid = false;
 		}
 
 		static internal::HandlePool<Object>& pool()
@@ -321,12 +294,88 @@ namespace oly
 		{
 			for (size_t idx : marked_for_deletion)
 			{
-				for (Handle<Object>* handle : references[idx])
-					handle->valid = false;
-				references[idx].clear();
+				while (reference_heads[idx])
+				{
+					reference_heads[idx]->prev_adj_reference = nullptr;
+					Handle<Object>* next = reference_heads[idx]->next_adj_reference;
+					reference_heads[idx]->next_adj_reference = nullptr;
+					reference_heads[idx] = next;
+				}
 			}
 
 			marked_for_deletion.clear();
+		}
+
+		template<typename Object>
+		inline void HandlePool<Object>::increment_references(size_t idx, Handle<Object>* handle)
+		{
+			if (reference_heads[idx])
+			{
+				Handle<Object>* next = reference_heads[idx]->next_adj_reference;
+				reference_heads[idx]->next_adj_reference = handle;
+				handle->prev_adj_reference = reference_heads[idx];
+				handle->next_adj_reference = next;
+				if (next)
+					next->prev_adj_reference = handle;
+			}
+			else
+				reference_heads[idx] = handle;
+		}
+
+		template<typename Object>
+		inline void HandlePool<Object>::decrement_references(size_t idx, Handle<Object>* handle)
+		{
+			Handle<Object>* prev = handle->prev_adj_reference;
+			Handle<Object>* next = handle->next_adj_reference;
+			
+			if (next == prev)
+			{
+				if (next)
+				{
+					next->prev_adj_reference = nullptr;
+					next->next_adj_reference = nullptr;
+				}
+			}
+			else
+			{
+				if (next)
+					next->prev_adj_reference = prev;
+				if (prev)
+					prev->next_adj_reference = next;
+			}
+			
+			if (reference_heads[idx] == handle)
+				reference_heads[idx] = next;
+			
+			handle->prev_adj_reference = nullptr;
+			handle->next_adj_reference = nullptr;
+
+			if (!reference_heads[idx])
+			{
+				marked_for_deletion.erase(idx);
+				unoccupied.push(idx);
+			}
+		}
+
+		template<typename Object>
+		inline void HandlePool<Object>::swap_references(size_t idx, Handle<Object>* existing, Handle<Object>* with)
+		{
+			Handle<Object>* prev = existing->prev_adj_reference;
+			Handle<Object>* next = existing->next_adj_reference;
+
+			existing->prev_adj_reference = nullptr;
+			existing->next_adj_reference = nullptr;
+			
+			with->prev_adj_reference = prev;
+			with->next_adj_reference = next;
+			
+			if (prev)
+				prev->next_adj_reference = with;
+			if (next)
+				next->prev_adj_reference = with;
+			
+			if (reference_heads[idx] == existing)
+				reference_heads[idx] = with;
 		}
 	}
 }
