af12beeb6a1fcc55731071ceb7db1a7c125d7101
RyanDeBarros <ryan2004db@gmail.com>
2026-09-17 22:18:53 -0400

    Use imp::stl_hash in Kerning map

--- engine/graphics/text/Kerning.h
+++ engine/graphics/text/Kerning.h
@@ -1,20 +1,17 @@
 #pragma once
 
+#include <imp/hash.hpp>
 #include <imp/utf.hpp>
 
 #include <unordered_map>
 
 namespace oly::rendering
 {
-    // TODO v9.3 use imp::stl_hash
-	struct CodepointPairHash
-	{
-		size_t operator()(const std::pair<imp::utf::codepoint, imp::utf::codepoint>& p) const { return std::hash<int>{}(p.first) ^ (std::hash<int>{}(p.second) << 1); }
-	};
-
 	struct Kerning
 	{
-		typedef std::unordered_map<std::pair<imp::utf::codepoint, imp::utf::codepoint>, int, CodepointPairHash> Map; // maps pairs of glyphs to kerning spacing
-		Map map;
+        // maps pairs of glyphs to kerning spacing
+		typedef std::unordered_map<std::pair<imp::utf::codepoint, imp::utf::codepoint>, int, imp::stl_hash<>> Map;
+		
+        Map map;
 	};
 }
